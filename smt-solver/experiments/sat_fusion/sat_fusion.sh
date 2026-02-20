357f2b404f901077e12f674fce34b3130aa6bd76
jCabala <kuba.cabala6@gmail.com>
2026-02-20 17:32:12 +0000

    feat: Contenerized the smt-solver experiments

--- smt-solver/experiments/sat_fusion/sat_fusion.sh
+++ smt-solver/experiments/sat_fusion/sat_fusion.sh
@@ -1,20 +1,22 @@
-SCRIPT_DIR=$(dirname -- "$0")
-cd $SCRIPT_DIR/
+#!/usr/bin/env bash
+set -euo pipefail
 
-## CONFIGURATION
-ORACLE=sat # sat or unsat
-BENCHMARKS=$SCRIPT_DIR/../../benchmarks/SMT-benchmarks/core/sat/
-TIMEOUT=30 # seconds
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+source "$SCRIPT_DIR/../common.sh"
+
+ORACLE="sat" # sat or unsat
+BENCHMARKS="$SCRIPT_DIR/../../benchmarks/SMT-benchmarks/core/sat/"
+TIMEOUT="30" # seconds
 SOLVER="picus" # z3 or cvc5 or picus
-PRUNE=2
+PRUNE="2"
 DSL="circom" # gnark or circom
 CONFIG="./sat_fusion_config.txt"
-PRUNE_SEED=42
+PRUNE_SEED="42"
 
-# If object directory does not exist, create it
-if [ ! -d ./obj ]; then
-    mkdir ./obj
-fi
+CLI_COMMAND="python3.11 /workspace/smt-solver/cli.py solve --zk-dsl $DSL --solver $SOLVER --prune $PRUNE --prune-seed $PRUNE_SEED --tmp-dir /workspace/smt-solver/experiments/tmp_fusion/"
+YY_CONFIG="$CONFIG"
+OUT_FILE="./obj/sat_fusion.out"
 
-CLI_COMMAND="../../cli.py solve --zk-dsl $DSL --solver $SOLVER --prune $PRUNE --prune-seed $PRUNE_SEED --tmp-dir ../tmp_fusion/"
-yinyang "$CLI_COMMAND" --oracle $ORACLE --timeout $TIMEOUT -c $CONFIG --l ./obj/logs --s ./obj/scratch --b ./obj/bugs $BENCHMARKS > sat_fusion.out 2>&1
+run_in_podman_if_needed "sat_fusion/sat_fusion.sh"
+cd "$SCRIPT_DIR"
+run_yinyang_experiment
