7f24de085e632bac792c547717f8d275567d3cc6
jCabala <kuba.cabala6@gmail.com>
2026-03-24 12:07:11 +0100

    feat: Propagated the hints flag to the experiments

--- smt-solver/experiments/picus_fusion/picus_fusion.sh
+++ smt-solver/experiments/picus_fusion/picus_fusion.sh
@@ -14,6 +14,7 @@ FUSION_SIDE_POLICY="one"
 SOLVER="picus"
 CONFIG="./picus_fusion_config.txt"
 YY_SEED="5959"
+WITH_HINTS="" # e.g. --with-hints
 CONTAINER_MEMORY="${CONTAINER_MEMORY:-64g}"
 CONTAINER_MEMORY_SWAP="${CONTAINER_MEMORY_SWAP:--1}"
 
@@ -81,7 +82,7 @@ DSL="${2:-gnark}"          # gnark|circom
 RUN_LABEL="${3:-$DSL}"     # used for output/log folder names
 TMP_DIR="${TMP_DIR:-/workspace/smt-solver/experiments/tmp_fusion}"
 
-CLI_COMMAND="python3 /workspace/smt-solver/cli.py solve --zk-dsl $DSL --solver $SOLVER --tmp-dir $TMP_DIR"
+CLI_COMMAND="python3 /workspace/smt-solver/cli.py solve --zk-dsl $DSL --solver $SOLVER --tmp-dir $TMP_DIR $WITH_HINTS"
 YY_CONFIG="$CONFIG"
 OUT_FILE="./obj/picus_fusion_${RUN_LABEL}.out"
 YY_LOG_DIR="./obj/${RUN_LABEL}/logs"
